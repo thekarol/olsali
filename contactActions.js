a8fe12c56b03d1277be87bd535b42138071ed4d8
thekkarol <thekkarol@gmail.com>
2022-05-17 19:46:31 +0200

    contact

---
+++ contactActions.js
@@ -0,0 +1,11 @@
+function showContact() {
+    var x = document.getElementById("contact");
+    x.style.visibility = "visible";
+    x.style.opacity = 1;
+}
+
+function hideContact() {
+    var x = document.getElementById("contact");
+    x.style.visibility = "hidden";
+    x.style.opacity = 0;
+}
